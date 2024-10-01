d9cdfa20ebd875e5da436594ce597b8be1577ce1
sabriades <sabrinaanna.desiderio@gmail.com>
2024-10-01 22:19:30 +0200

    simple stuff
    basics

---
+++ simple_calculator.py
@@ -0,0 +1,8 @@
+first_value=input('First ')
+print('first value is '+first_value)
+second_value=input('Second ')
+print('second value is '+second_value)
+first_int=int(first_value)
+second_int=int(second_value)
+sum=first_int+second_int
+print(sum)
---
+++ simple_exercises.py
@@ -0,0 +1,25 @@
+print('cominciamo')
+#variabili
+anni=28
+print('io ho',anni, "anni")
+nome='Riccardo'
+is_online=True
+
+#ex1
+patient='John Smith'
+age=20
+type='new patient'
+print(patient,age,type)
+
+##ex2
+name=input('what is ur name? ')
+print('Hello, '+name) #concatenazione di stringhe
+#example of the use of the input function in python
+birth_year=input('Enter year of birth: ')
+int_birth_year=int(birth_year)
+age=2024-int_birth_year
+print(age)
+age_str=str(age)
+print('your age is '+age_str)
+
+
