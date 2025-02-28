00be9c6c1c64f42d6cbe240b61aa361cc11846e5
sabriades <sabrinaanna.desiderio@gmail.com>
2025-02-28 13:17:56 +0100

    second

---
+++ p2.py
@@ -0,0 +1,7 @@
+first=input("first: ")
+second=input("second: ")
+first=float(first)
+second=float(second)
+sum=first+second
+sum=str(sum)
+print("sum: "+sum)
--- rip.py
+++ rip.py
@@ -4,3 +4,20 @@ print(age) #prints the value of the variable
 my_sister="Pesca"
 #declare a variable
 is_at_home=False #False is boolean False value
+
+name=input("what's your name? ") #requires a input
+#function input returnes the value that we entered in the terminal window
+#let's store this value: name
+print("Hello "+name+"!") #string concatenation
+birth_year=input("pls insert your birth year ")
+#string convertion to int:
+birth_year=int(birth_year)
+age=2025-birth_year
+age=str(age)
+print("your age is "+age)
+#if we have a float, we use float()
+#function to convert the type of the variable
+#int()
+#float()
+#bool()
+#str()
