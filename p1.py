b44910cdbc1c7785b1f2dfe3245f7fa4c5960d0c
sabriades <sabrinaanna.desiderio@gmail.com>
2025-02-28 12:41:14 +0100

    new push
    it's been a minute

---
+++ p1.py
@@ -0,0 +1,8 @@
+patient_name="John Smith"
+patient_age=20
+new_patient=True
+print("Name:",patient_name)
+print("Age:",patient_age)
+if new_patient!=False:
+    print(patient_name,"is a new patient")
+else: print (patient_name, "is not a new patient")
---
+++ rip.py
@@ -0,0 +1,6 @@
+print("I'm revising my Python skills. It's been a minute since I used Python")
+age=20 #state a variable
+print(age) #prints the value of the variable
+my_sister="Pesca"
+#declare a variable
+is_at_home=False #False is boolean False value
